7f8eb444c721f8fef521ce7afb9c90d4cee3c2a1
Ben Peachey <potherca@gmail.com>
2015-04-27 22:47:20 +0200

    Various improvements to the build script
    - Renames environmental variable to reflect the project name
    - Skippes the source repo that triggered the build
    - Refactors functions to adhere more to the rule to "do one thing well"

--- build.sh
+++ build.sh
@@ -2,44 +2,47 @@
 
 set -e
 set -u
+# set -x
 
 # ==============================================================================
 # The following variables are required and need to be set to the environment by
 # the user:
 #
-# GH_PAGES_TARGET_REPO
-# GH_PAGES_SOURCE_REPOS
+# NEWCOMEN_TARGET_REPO
+# NEWCOMEN_SOURCE_REPOS
 # GH_TOKEN
 #
 # The following variables are optional, default values have been provided:
 #
-# IW_AUTHOR_NAME
-# IW_AUTHOR_EMAIL
+# NEWCOMEN_AUTHOR_NAME
+# NEWCOMEN_AUTHOR_EMAIL
 #
 # ==============================================================================
-readonly sApplicationName='InstantWebsite'
+readonly sApplicationName='Newcomen'
 
 sGitUser='potherca-bot'
 sGitMail='potherca+bot@gmail.com'
 
-sTargetRepo=''
 sGithubToken=''
+sSourceRepo=''
+sTargetRepo=''
 
 declare -a aSourceRepos
+# ==============================================================================
 
-# sed -l basically makes sed replace and buffer through stdin to stdout
-# so you get updates while the command runs and dont wait for the end
-# e.g. npm install | indent
 function indent() {
-  # if an arg is given it's a flag indicating we shouldn't indent the first line,
-  # so use :+ to tell SED accordingly if that parameter is set, otherwise null
-  # string for no range selector prefix (it selects from line 2 onwards and then
-  # every 1st line, meaning all lines)
-  local c="${1:+"2,999"} s/^/       /"
-  case $(uname) in
-    Darwin) sed -l "$c";; # mac/bsd sed: -l buffers on line boundaries
-    *)      sed -u "$c";; # unix/gnu sed: -u unbuffered (arbitrary) chunks of data
-  esac
+    # sed -l basically makes sed replace and buffer through stdin to stdout
+    # so you get updates while the command runs and dont wait for the end
+    # e.g. npm install | indent
+    # if an arg is given it's a flag indicating we shouldn't indent the first line,
+    # so use :+ to tell SED accordingly if that parameter is set, otherwise null
+    # string for no range selector prefix (it selects from line 2 onwards and then
+    # every 1st line, meaning all lines)
+    local c="${1:+"2,999"} s/^/       /"
+    case $(uname) in
+        Darwin) sed -l "$c";; # mac/bsd sed: -l buffers on line boundaries
+        *)      sed -u "$c";; # unix/gnu sed: -u unbuffered (arbitrary) chunks of data
+    esac
 }
 
 function printError() {
@@ -58,8 +61,8 @@ function printStatus() {
 }
 
 function setEnvironmentFromParamters() {
-    GH_PAGES_TARGET_REPO="$1"
-    GH_PAGES_SOURCE_REPOS="$2"
+    NEWCOMEN_TARGET_REPO="$1"
+    NEWCOMEN_SOURCE_REPOS="$2"
     GH_TOKEN="$3"
 }
 
@@ -67,12 +70,12 @@ function validateEnvironment() {
     local sErrorMessage=''
 
     set +u
-    if [ -z "${GH_PAGES_SOURCE_REPOS}" ];then
-        sErrorMessage="${sErrorMessage}\n - GH_PAGES_SOURCE_REPOS"
+    if [ -z "${NEWCOMEN_SOURCE_REPOS}" ];then
+        sErrorMessage="${sErrorMessage}\n - NEWCOMEN_SOURCE_REPOS"
     fi
 
-    if [ -z "${GH_PAGES_TARGET_REPO}" ];then
-        sErrorMessage="${sErrorMessage}\n - GH_PAGES_TARGET_REPO"
+    if [ -z "${NEWCOMEN_TARGET_REPO}" ];then
+        sErrorMessage="${sErrorMessage}\n - NEWCOMEN_TARGET_REPO"
     fi
 
     if [ -z "${GH_TOKEN}" ];then
@@ -86,24 +89,26 @@ function validateEnvironment() {
         printError "${sErrorMessage}"
         exit 65
     fi
-
 }
 
 function setVariables() {
-    IFS=',' read -ra aSourceRepos <<< "${GH_PAGES_SOURCE_REPOS}"
+    IFS=',' read -ra aSourceRepos <<< "${NEWCOMEN_SOURCE_REPOS}"
 
-    sTargetRepo="${GH_PAGES_TARGET_REPO}"
+    sTargetRepo="${NEWCOMEN_TARGET_REPO}"
 
     sGithubToken="${GH_TOKEN}"
 
-    if [ -n "$(echo ${IW_AUTHOR_NAME})" ]; then
-        sGitUser="${IW_AUTHOR_NAME}"
+    if [ -n "$(echo ${NEWCOMEN_AUTHOR_NAME})" ]; then
+        sGitUser="${NEWCOMEN_AUTHOR_NAME}"
     fi
 
-    if [ -n "$(echo ${IW_AUTHOR_EMAIL})" ]; then
-        sGitMail="${IW_AUTHOR_EMAIL}"
+    if [ -n "$(echo ${NEWCOMEN_AUTHOR_EMAIL})" ]; then
+        sGitMail="${NEWCOMEN_AUTHOR_EMAIL}"
     fi
+}
 
+function storeSource() {
+    sSourceRepo="$(git config --get remote.origin.url | cut -f2 -d':' | cut -f1 -d'.')"
 }
 
 function setGitUser() {
@@ -113,12 +118,26 @@ function setGitUser() {
     git config --global user.name "${sGitUser}"
 }
 
-function prepareRepository() {
-    local sGitRepo="$1"
+function addRepositoryContent() {
+    local sRepo="$1"
 
+    if [ "${sRepo}" == "${sSourceRepo}" ];then
+        printStatus "Skipping content from ${sRepo}"
+    else
+        printStatus "Adding content from ${sRepo}"
+
+        fetchRepositoryContent "${sRepo}" "${sBranch}"
+    fi
+}
+
+function prepareRepository() {
     rm -Rf .git
 
     git init | indent
+}
+
+function addRemoteToRepository() {
+    local sGitRepo="$1"
 
     if [ -z "${sGithubToken}" ];then
         git remote add origin "https://github.com/${sGitRepo}" | indent
@@ -127,37 +146,42 @@ function prepareRepository() {
     fi
 }
 
-function fetchRepositoryContent() {
-    local sGitRepo="$1"
-
-    #@CHECKME: If $sGitRepo is the repo the build was triggered for git should
-    #          report "Everything up to date". Can there be other side-effects?
-    printStatus "Fetching contents from ${sGitRepo}"
-    prepareRepository "${sGitRepo}"
-    git fetch | indent
-}
-
 function getBranch() {
     local sBranch='master'
 
     sBranchName='gh-pages'
 
-    if [ "$(git rev-parse --git-dir > /dev/null 2>&1)" ] && [ -n "$(git show-ref refs/heads/${sBranchName})" ]; then
+    if [ "$(git show-ref ${sBranchName} 2>&1)" ]; then
         sBranch="${sBranchName}"
     fi
 
     echo "${sBranch}"
 }
 
-function mergeContents() {
-    local sBranch="$1"
-    local sMergeBranch='instant-web-merge-branche'
+function commitContent() {
+    local sMergeBranch="$1"
 
     git checkout -b "${sMergeBranch}" | indent
     git add -A | indent
-    git commit -m "${sApplicationName}: Adding changes from source repositories." | indent
+    git commit -a -m "${sApplicationName}: Adding changes from source repositories." | indent
+}
+
+function fetchRepositoryContent() {
+    local sRepo="$1"
+
+    local sMergeBranch='newcomen-merge-branche'
+
+    prepareRepository
+
+    addRemoteToRepository "${sRepo}"
+
+    commitContent "${sMergeBranch}"
+
+    git fetch | indent
+
+    sBranch="$(getBranch)"
     git checkout "${sBranch}" | indent
-    git merge --strategy-option theirs "${sMergeBranch}" -m "${sApplicationName}: Merging content from source repositories." | indent
+    git merge --strategy=recursive --strategy-option=theirs "${sMergeBranch}" -m "${sApplicationName}: Merging content from source repositories." | indent
 }
 
 function pushContents() {
@@ -166,23 +190,20 @@ function pushContents() {
 }
 
 function runBuild() {
+    local sBranch=''
+
     printTopic 'Running build'
 
     setGitUser
+    storeSource
 
     printTopic 'Fetch content from source repositories'
     for sRepo in "${aSourceRepos[@]}"; do
-        fetchRepositoryContent "${sRepo}"
-        git pull origin master
+        addRepositoryContent "${sRepo}"
     done
 
-    printTopic 'Fetching content from target repository'
-    fetchRepositoryContent "${sTargetRepo}" "${GH_TOKEN}"
-
-    sBranch="$(getBranch)"
-
-    printTopic 'Merging content from source repositories in target repository'
-    mergeContents "${sBranch}"
+    printTopic 'Fetch content from target repository'
+    addRepositoryContent "${sTargetRepo}" "${sBranch}"
 
     printTopic 'Sending merged content to target repository'
     pushContents "${sBranch}"
